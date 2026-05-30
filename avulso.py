b35b4f0005950ff70455807fee37799bc46f4ea4
Lucas Yoshimura <lucasyoshimura07@gmail.com>
2026-05-30 16:48:22 -0300

    Add files via upload

---
+++ avulso.py
@@ -0,0 +1,8 @@
+nome = input("Digite seu nome completo: ")
+idade = int(input("Digite sua idade: "))
+altura = float(input("Digite sua altura em metros: "))
+
+print("Cadastro realizado com sucesso!")
+print(f"Nome: {nome}")
+print(f"Idade: {idade} anos")
+print(f"Altura: {altura} metros")
