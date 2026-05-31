0505ee3d8eb4da84f26cbf11ee75294fccaf51d4
Lucas Yoshimura <lucasyoshimura07@gmail.com>
2026-05-31 19:58:33 -0300

    Create 1.3.py
    Add a script to demonstrate list operations in Python.

---
+++ Estruturas_de_Dados/1.3.py
@@ -0,0 +1,18 @@
+notas = [7.5, 8.0, 6.5, 9.0, 7.0, 8.5, 6.0, 9.5]
+
+for item in notas:
+    print(item)  
+
+for x in range(len(notas)):
+    print(f"Índice {x}: {notas[x]}")  
+
+print(notas[0:3])
+
+print(notas[5:8])
+
+print(notas[0:8:2])
+
+print(notas[-1::-1])    
+
+
+
