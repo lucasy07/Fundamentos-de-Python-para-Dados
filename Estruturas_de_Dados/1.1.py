3eed91dfd1a884e869e8bb443a2c0dad28b97fee
Lucas Yoshimura <lucasyoshimura07@gmail.com>
2026-05-31 19:57:56 -0300

    Create 1.1.py

---
+++ Estruturas_de_Dados/1.1.py
@@ -0,0 +1,8 @@
+frutas = ["maçã", "banana", "laranja", "uva", "morango"]
+print(frutas[0])  
+print(frutas[-1]) 
+print(frutas[2]) 
+if "banana" in frutas:
+    print("A banana está na lista de frutas.")
+frutas[1] = "abacaxi"  
+print(frutas)  
