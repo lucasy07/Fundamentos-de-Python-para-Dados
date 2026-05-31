f235cd23b9c4a55d6980572f6bd79542658593e0
Lucas Yoshimura <lucasyoshimura07@gmail.com>
2026-05-31 19:59:08 -0300

    Create 2.1.py

---
+++ Estruturas_de_Dados/2.1.py
@@ -0,0 +1,13 @@
+coordenadas = (10, 20, 30)
+print(f"Tupla: coordenadas")
+
+print(f"Tipo de variável: {type(coordenadas)}")
+
+print(f"Primeiro elemento: {coordenadas[0]}")
+
+print(f"Último elemento: {coordenadas[-1]}")
+
+if 20 in coordenadas:
+    print("20 está na Tupla")
+
+print(f"Tamanho da tupla: {len(coordenadas)}")
