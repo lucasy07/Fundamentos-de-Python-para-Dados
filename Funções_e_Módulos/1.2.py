c52496fb68aceebe38a961488e5a02130ba04da0
Lucas Yoshimura <lucasyoshimura07@gmail.com>
2026-06-03 16:35:16 -0300

    Create 1.2.py

---
+++ Funções_e_Módulos/1.2.py
@@ -0,0 +1,7 @@
+def calcular_area_retangulo():
+    largura = int(input())
+    altura = int(input())
+    resultado = largura * altura
+    print(f"A área do retângulo é: {resultado}")
+
+calcular_area_retangulo()
